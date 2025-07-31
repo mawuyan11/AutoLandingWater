f72b5ceff0d612a1f5dc12d22c99313ae286148a
ma_wuyan11 <ma_wuyan@163.com>
2025-07-31 14:32:38 +0800

    first commit

---
+++ src/main/java/com/ma_wuyan/autolandingwater/AutoLandingWater.java
@@ -0,0 +1,25 @@
+package com.ma_wuyan.autolandingwater;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AutoLandingWater implements ModInitializer {
+	public static final String MOD_ID = "auto-landing-water";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// 安全地检查开发环境标志
+		boolean isDevelopment = Boolean.parseBoolean(
+				System.getProperty("fabric.development", "false")
+		);
+
+		if (isDevelopment) {
+			LOGGER.info("AutoLandingWater mod initialized with DEBUG logging");
+		} else {
+			LOGGER.info("AutoLandingWater mod initialized");
+		}
+	}
+}
