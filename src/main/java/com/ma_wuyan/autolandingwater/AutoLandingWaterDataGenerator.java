305771e50c2fd0d84d17375989611e75d3e17e59
mawuyan11 <ma_wuyan@163.com>
2025-08-29 22:20:42 +0800

    Initial commit: AutoLandingWater mod

---
+++ src/main/java/com/ma_wuyan/autolandingwater/AutoLandingWaterDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ma_wuyan.autolandingwater;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AutoLandingWaterDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
